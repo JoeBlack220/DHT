36b2f377a5817725ada0c299eca6bd8d35df0327
Yichun <yan00104@umn.edu>
2019-03-30 13:50:42 -0500

    first commit

---
+++ thrift/dht.thrift
@@ -0,0 +1,17 @@
+struct NodeInfo {
+  1:string nodeIp,
+  2:string nodePort,
+  3:i32 nodeKey
+}
+
+service SNodeService {
+  NodeInfo join(1: string ip,2: string port),
+  NodeInfo postJoin(1: string ip, 2: string port),
+  NodeInfo getNode() 
+}
+
+service NodeService {
+  bool setItem(1: string bookTitle, 2: string gerne),
+  string getItem(1: string bookTitle),
+  bool UpdateDHT()
+}
